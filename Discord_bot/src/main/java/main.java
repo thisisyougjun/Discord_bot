1f5c05c519086ebba4c30ed86b106815bd08debe
thisisyoungjun <122899@naver.com>
2021-08-26 22:25:47 +0900

    commit

---
+++ Discord_bot/src/main/java/main.java
@@ -0,0 +1,31 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import okhttp3.EventListener;
+
+import javax.security.auth.login.LoginException;
+
+public class main extends ListenerAdapter {
+
+    public static void main(String[] args) throws LoginException {
+
+        // 기본 jda를 만들고
+        JDA jda = JDABuilder.createDefault("ODY5OTE1OTIwOTM4NzA0OTU4.YQFKVQ.7Ash4KGCDmIvtygl11L2OexiU90").build();
+
+        // jda에 이벤트를 감지하는 리스너를 넣는다.
+        jda.addEventListener(new main());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        // 받은 메세지 내용이 !ping이라면
+        if (event.getMessage().getContentRaw().equals("!ping")) {
+            // pong라는 내용을 보낸다.
+            event.getChannel().sendMessage("pong!").queue();
+        }
+    }
+}
